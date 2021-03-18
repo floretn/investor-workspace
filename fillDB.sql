924ce3a9eae80ccc8393a1cb3d1aeb15d599da63
floretn <ivanse99@yandex.ru>
2021-03-18 17:47:46 +0300

    remove fill DOIC in fillDB

--- fillDB.sql
+++ fillDB.sql
@@ -2,6 +2,5 @@
 
 insert into investor_workspace.t_indexes (indx_name) values ('IMOEX');
 insert into investor_workspace.t_currency (cur_name) values ('Рубли');
-insert into investor_workspace.t_date_of_indexes_changes (doic_indx_fk, doic_date_chng) values (1, CURRENT_DATE);
 
 end;
